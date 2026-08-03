4312e7f38f739adac72c31feb89661aee253e450
cadavezne-creator <cadavezne@gmail.com>
2026-08-03 09:51:41 +0300

    zaebali

--- module5/hw11/abc.py
+++ module5/hw11/abc.py
@@ -1,32 +1,64 @@
-class Uaalphabet:
-# создали класс uaalphabet
+class Alphabet:
     def __init__(self, lang, letters):
         self.lang = lang
         self.letters = letters
 
-    def print(self): 
-        # Вівели букви та мову     
-        print(f"Language: {self.lang}")
-        print(f"Letters: {self.letters}")
+    def letters_num(self):
+        return len(self.letters)
 
-    def letters_num(self): 
-        # Вивели кількість букв
-        return len(self.letters)    
+    def is_alphabet_letter(self, letters, letter):
+        return "О, так" if letter.lower() in letters else "О, ніт!!!"
 
+class UaAlphabet(Alphabet):
+    def __init__(self):
+        super().__init__("Українська", list("абвгдеєжзиіїйклмнопрстуфхцчшщьюя"))
 
+    def is_ua_letter(self, letter):
+        return self.is_alphabet_letter(self.letters, letter)
 
+class EngAlphabet(Alphabet):
+    def __init__(self):
+        super().__init__("English", list("abcdefghijklmnopqrstuvwxyz"))
+
+    def is_en_letter(self, letter):
+        return self.is_alphabet_letter(self.letters, letter)
+
+    @staticmethod
+    def example():
+        return "The quick brown fox jumps over the lazy dog."
 
 # Заполняем класс данніми и буквами
-Uaalphabet = Uaalphabet("Українська", ["А", "Б", "В", "Г", "Д", "Е", "Є", "Ж", "З", "И", "І", "Ї", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ь", "Ю", "Я"])
+uaAlphabet = UaAlphabet()
 
+# Заполняем класс данніми и буквами
+engAlphabet = EngAlphabet()
 
 # Тестируем - віводим мову
-print(Uaalphabet.lang)
+print(uaAlphabet.lang)
 
 # Тестируем - віводим букви
-print(Uaalphabet.letters)
+print(uaAlphabet.letters)
 
 # Тестируем - сщздаем переменную и говорим что она равна методу letters_num
-letters_count = Uaalphabet.letters_num()
+letters_count = uaAlphabet.letters_num()
 print(f"Всього букв: {letters_count}")
 
+# Тестируем - віводим мову
+print(engAlphabet.lang)
+
+# Тестируем - віводим букви
+print(engAlphabet.letters)
+
+# Тестируем - сщздаем переменную и говорим что она равна методу letters_num
+letters_count = engAlphabet.letters_num()
+print(f"Всього букв: {letters_count}")
+
+# Перевіряємо, чи належить літера  англійському алфавіту
+# while(True):
+name = input("Введіть англійську літеру: ")
+print(engAlphabet.is_en_letter(name))
+
+# Перевіряємо, чи належить літера  українському алфавіту
+# while(True):
+name = input("Введіть українську літеру: ")
+print(uaAlphabet.is_ua_letter(name))
